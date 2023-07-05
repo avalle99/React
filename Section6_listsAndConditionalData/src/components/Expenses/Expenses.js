36cd8c91822ecd0a1497b4495f8c5d39df2b96be
avalle99 <alejandro.valle@uvltd.tech>
2023-07-05 17:49:14 -0600

    Update

---
+++ Section6_listsAndConditionalData/src/components/Expenses/Expenses.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+
+export default function Expenses(props) {
+  console.log(props.items);
+  const expenses = props.items.map((item) => {
+    return (
+      <ExpenseItem title={item.title} amount={item.amount} date={item.date} />
+    );
+  });
+
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const FilterChangeHandler = (selectedYear) => {
+    // console.log("Expenses.js", selectedYear);
+    setFilteredYear(selectedYear);
+  };
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        defaultValue={filteredYear}
+        onChangeFilter={FilterChangeHandler}
+      />
+      {expenses}
+      {/* <ExpenseItem
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
+      ></ExpenseItem> */}
+    </Card>
+  );
+}
